60d396414d343cdfe44a3569988b3e5f818e495c
Caio Lopes <caio.lopes2@fatec.sp.gov.br>
2024-11-29 18:07:46 -0300

    commit: test launcher

---
+++ app/assets/js/distromanager.js
@@ -0,0 +1,17 @@
+const { DistributionAPI } = require('helios-core/common')
+
+const ConfigManager = require('./configmanager')
+
+// Old WesterosCraft url.
+// exports.REMOTE_DISTRO_URL = 'https://frostisnotsafe.github.io/NebulaHost-CosmoRP/distribution.json'
+exports.REMOTE_DISTRO_URL = 'https://frostisnotsafe.github.io/NebulaHost-CosmoRP/distribution.json'
+
+const api = new DistributionAPI(
+    ConfigManager.getLauncherDirectory(),
+    null, // Injected forcefully by the preloader.
+    null, // Injected forcefully by the preloader.
+    exports.REMOTE_DISTRO_URL,
+    false
+)
+
+exports.DistroAPI = api
